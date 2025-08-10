5490cc251a624e11e302ac08da6466ec7d2324c3
MikiEremiki <mr.eremin.ma@ya.ru>
2025-08-10 08:57:47 +0300

    refactor: улучшена установка команд бота
    - Вынесена команда `/start` в отдельную переменную для повторного использования.

--- src/utils/setup_cmds.py
+++ src/utils/setup_cmds.py
@@ -7,13 +7,15 @@ from config.config import Config
 async def setup_commands(bot, config: Config):
     await bot.delete_my_commands(BotCommandScopeDefault())
     balance_cmd = BotCommand(command='balance', description='Отчет по балансам')
+    start_cmd = BotCommand(command='start', description='Старт/Перезапуск бота')
     await bot.set_my_commands([
-        BotCommand(command='start', description='Старт/Перезапуск бота'),
+        start_cmd,
         balance_cmd,
     ],
         scope=BotCommandScopeDefault()
     )
     await bot.set_my_commands([
+        start_cmd,
         balance_cmd,
         BotCommand(command='settings', description='Настройки бота')
     ],
